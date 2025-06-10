82ceb98e99b2b7576547a373bf7586d0bb2227ca
Timeness <careless@flash.co>
2025-06-10 16:54:20 +0530

    Update Fetch.cs

--- modules/Fetch.cs
+++ modules/Fetch.cs
@@ -1,18 +1,20 @@
+using System;
 using System.Net.Http;
 using System.Text.Json;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace InfixBot.modules
+namespace InfixBot
 {
-    public static class Fetch
+    public class Fetch
     {
-        private static readonly HttpClient client = new HttpClient();
-        public const string API_URL = "https://infixchain.vercel.app";
+        private static readonly HttpClient _httpClient = new HttpClient();
+        public const string API_URL = "https://infixchain-api.vercel.app";
 
         public static async Task<JsonElement> GetAsync(string endpoint, Dictionary<string, string> parameters = null)
         {
             var query = parameters != null ? $"?{string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"))}" : "";
-            var response = await client.GetAsync($"{API_URL}/{endpoint}{query}");
+            var response = await _httpClient.GetAsync($"{API_URL}/{endpoint}{query}");
             response.EnsureSuccessStatusCode();
             var content = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<JsonElement>(content);
